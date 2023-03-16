1dd84d9441152cdb878cc3e55c53eda9c96406a2
Adrián <123007421+Kirk810@users.noreply.github.com>
2023-03-16 19:38:24 +0100

    feaure-middleware

---
+++ src/utils/token.js
@@ -0,0 +1,15 @@
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const createToken = (id, username) => {
+    return jwt.sign({ id, username }, process.env.JWT_SECRET, {
+        expiresIn: "1d",
+    });
+};
+
+const verifyToken = (token) => {
+    return jwt.verify(token, process.env.JWT_SECRET);
+};
+
+module.exports = { createToken, verifyToken };
